779a8adaba56229134a4e1eefa15f3e8b41659d0
neerajahirwar225 <neerajahirwar225@gmail.com>
2026-07-26 22:56:51 +0530

    Update API URL for production

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL:'http://localhost:5000/api',
+  // baseURL:'http://localhost:5000/api',
+  baseURL:'https://nyayaai-qbm5.onrender.com',
+  
   headers: {
     'Content-Type': 'application/json',
   },
