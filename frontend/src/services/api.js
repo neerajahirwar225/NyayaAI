607bdf24061b6a54f7b57c48c27c6803a6e0d485
neerajahirwar225 <neerajahirwar225@gmail.com>
2026-07-26 23:24:41 +0530

    Fix API base URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   // baseURL:'http://localhost:5000/api',
-  baseURL:'https://nyayaai-qbm5.onrender.com',
+  baseURL:'https://nyayaai-qbm5.onrender.com/api',
   
   headers: {
     'Content-Type': 'application/json',
